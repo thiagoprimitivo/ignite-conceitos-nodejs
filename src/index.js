277cfff576da06ae577ead4e975f18f3f710c79a
Thiago Primitivo de Oliveira <thiago.primitivo@gmail.com>
2021-04-14 07:06:43 -0300

    Criação do todo

--- src/index.js
+++ src/index.js
@@ -48,7 +48,20 @@ app.get('/todos', checkExistsUserAccount, (request, response) => {
 });
 
 app.post('/todos', checkExistsUserAccount, (request, response) => {
+    const { title, deadline } = request.body;
+    const { user } = request;
 
+    const todo = {
+        id: uuidv4(),
+        title,
+        done: false,
+        deadline: new Date(deadline),
+        created_at: new Date()
+    }
+
+    user.todos.push(todo);
+
+    return response.status(201).send();
 });
 
 app.put('/todos/:id', checkExistsUserAccount, (request, response) => {
