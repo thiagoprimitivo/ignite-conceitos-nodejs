a6f9673be7214644644deb87015a7973cd37a75d
Thiago Primitivo de Oliveira <thiago.primitivo@gmail.com>
2021-04-14 06:55:55 -0300

    Criação do usuário

--- src/index.js
+++ src/index.js
@@ -1,7 +1,48 @@
 const express = require('express');
+const cors = require('cors');
+
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
+const users = [];
+
+function checkExistsUserAccount(request, response, next) {
+    
+}
+
+app.use(cors());
 app.use(express.json());
+
+app.post('/users', (request, response) => {
+    const { name, username } = request.body;
+
+    const userAlreadyExists = users.some((user) => user.username === username);
+
+    if (userAlreadyExists) {
+        return response.status(400).json({ error: 'User already exists!' });
+    }
+
+    users.push({
+        id: uuidv4(),
+        name,
+        username,
+        todos: []
+    });
+
+    return response.status(201).send();
+});
+
+app.get('/todos', checkExistsUserAccount, (request, response) => {
+
+});
+
+app.post('/todos', checkExistsUserAccount, (request, response) => {
+
+});
+
+app.put('/todos/:id', checkExistsUserAccount, (request, response) => {
+
+});
 
 app.listen(3333);
