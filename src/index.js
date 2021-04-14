2e8f82d71dfa2e2738f86582df65f6cead977adf
Thiago Primitivo de Oliveira <thiago.primitivo@gmail.com>
2021-04-14 06:56:35 -0300

    Validação existência usuário

--- src/index.js
+++ src/index.js
@@ -8,7 +8,17 @@ const app = express();
 const users = [];
 
 function checkExistsUserAccount(request, response, next) {
-    
+    const { username } = request.headers;
+
+    const user = users.find((user) => user.username === username);
+
+    if (!user) {
+        return response.status(400).json({ error: 'User not found' });
+    }
+
+    request.user = user;
+
+    return next();
 }
 
 app.use(cors());
